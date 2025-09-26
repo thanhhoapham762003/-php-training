117a26be3079e611919a34aab0242e394e2ae6cb
thanhhoapham762003 <163566052+thanhhoapham762003@users.noreply.github.com>
2025-09-26 16:18:50 +0700

    Delete sources directory

--- sources/11-training-php/configs/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'web-mysql');   // service name trong docker-compose
-define('DB_USER', 'root');        // hoặc 'user' nếu bạn muốn dùng user thường
-define('DB_PASSWORD', 'pass');    // trùng với MYSQL_ROOT_PASSWORD trong docker-compose
-define('DB_PORT', 3306);
-define('DB_NAME', 'app_web1');    // đúng với tên DB bạn đã import
--- sources/11-training-php/helpers/xss.php
+++
@@ -1,4 +0,0 @@
-<?php
-function e($str) {
-    return htmlspecialchars($str ?? '', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-}
